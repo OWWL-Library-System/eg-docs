1d84d7d3a3bb1adc3f9dcf0734024814fd0734c4
danguarracino <18332275+danguarracino@users.noreply.github.com>
2025-02-10 16:12:17 -0500

    Specify / increase TOC heading levels

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -82,6 +82,10 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 5,
+    },
     navbar: {
       title: 'OWWL Technical Documentation',
       logo: {
