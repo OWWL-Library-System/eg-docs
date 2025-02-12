c7a9bab10a82c255f9523046145ad79f9e70ff91
danguarracino <18332275+danguarracino@users.noreply.github.com>
2025-02-12 09:03:15 -0500

    Set URL for netlify deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/cropped-OWWL-favicon-1-32x32.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://shimmering-naiad-ca07fd.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
